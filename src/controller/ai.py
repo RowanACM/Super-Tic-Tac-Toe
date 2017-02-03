4a096febb3bb183a92d8a49d5fa968571f56adc1
Nick LaPosta <lapost48@gmail.com>
2017-02-03 15:27:01 -0500

    Basic pseudocode for minimax

--- src/controller/ai.py
+++ src/controller/ai.py
@@ -1,8 +1,31 @@
 import sys
 
-def main(args):
-    print "Test"
-    pass
+class Node():
+	def __init__(self):
+		pass
+
+
+def minimax(node, depth, maximizingPlayer):
+	if depth == 0 or node.children is None:
+		return h(node)
+	
+	if maximizingPlayer:
+		bestValue = -1 - sys.maxint
+		for child in node.children:
+			v = minimax(child, depth-1, FALSE)
+			bestValue = v if (v > bestValue) else bestValue
+		return bestValue
+	else:
+		bestValue = sys.maxint
+		for child in node.children:
+			v = minimax(child, depth - 1, TRUE)
+			bestValue = v if (v < bestValue) else bestValue
+		return bestValue
+
+def h(node):
+	pass
 
 if __name__=="__main__":
-    main(sys.argv)
+	args = sys.argv
+	# Make args useful here
+	minimax()
